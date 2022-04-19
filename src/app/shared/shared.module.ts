c8f0aec75348c67f5f3c93ccee84306ffbfb2a90
tripathi-suraj <surajtripathi781@gmail.com>
2022-04-19 20:57:54 +0530

    2 files modified

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 
-const matmodule = [MatToolbarModule, MatSliderModule];
+const matmodule = [MatToolbarModule, MatSliderModule, MatIconModule];
 const modules = [FormsModule];
 
 @NgModule({
